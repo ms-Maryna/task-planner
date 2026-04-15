e936ec294212c5a78f96aeb926cd68fc2307fbb8
Merchant <128327771+merchantguildmember@users.noreply.github.com>
2026-04-15 16:38:47 +0100

    fixed registering redirection bug where it required a verification

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return redirect()->route('tasks.index');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
